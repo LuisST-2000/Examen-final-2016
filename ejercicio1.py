52e63a2f7bc41cbe46500a6f915f13c96f4245e0
LuisSalvador <50873596+Dextro0x@users.noreply.github.com>
2019-05-27 11:15:26 -0500

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,7 +1,22 @@
-for i in range(2,1000):
-    
-    if  i % 1 == 0 and i %i ==0:
-        print(i)
+def es_primo(num):
+    if num < 2:
+        return False
+    for i in range(2,num):
+        if num % i == 0:
+            return False
+        else:
+            return True
+
+def primos(num1,num2):
+    cont = 0
+    for i in range(num1,num2):
+        if es_primo(i) == True:
+            cont +=1
+
+    print("Hay", cont, "numeros primos")
+
+es_primo(1000)
+primos(1,1000) 
 
         
     
